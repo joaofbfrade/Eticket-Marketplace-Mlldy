adfcbaead4f3b565c4e8bce6616afc42a4cb876b
joaofbfrade <joaofbfrade97@gmail.com>
2022-10-15 18:27:27 +0100

    ja meti o moralis a funcionar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { CryptoCards, Button } from '@web3uikit/core';
+import { ConnectButton } from '@web3uikit/web3';
+import { MoralisProvider } from "react-moralis";
 
 function App() {
   return (
@@ -10,14 +13,17 @@ function App() {
        
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            
+            
+           <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
-        
+          <ConnectButton />
       </Navbar>
+    
 
-      
+   
     </div>
   );
 }
