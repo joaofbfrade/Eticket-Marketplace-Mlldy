a04875924e10fc432a167711ac635a95967c5e72
Pedro Bingre Miguel <pedro.bingre.miguel@gmail.com>
2022-10-17 17:12:07 +0100

    Navbar overwrite

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -7,6 +7,9 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { ConnectButton } from '@web3uikit/web3';
 
+import '../overWrite.css'; 
+
+import logo from '../assets/LogoDark.png';
 
 const imgstyle = {
   'padding-left': 20,
@@ -17,55 +20,67 @@ const expand = 'md';
 function Navbar2() {
   return (
     <>
-   
+    
       <Navbar key={expand} bg="dark"  variant="dark" expand={'md'} className="mb-3">
         <Container fluid>
-        <Navbar.Brand  href="#home">
+          <Navbar.Brand  href="#home">
             <img style={imgstyle}
-              src="https://mellody.art/wp-content/uploads/2022/05/Logo-Branco-Mellody-1-e1652910561504.png"
-              width="100"
-              height="30"
-              className="d-inline-block align-top"
-              alt=""
+            src={"https://mellody.art/wp-content/uploads/2022/05/Logo-Branco-Mellody-1-e1652910561504.png"}
+            width="120"
+            height="36"
+            className="d-inline-block align-top"
+            alt=""
             />
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+
           <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-${expand}`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-            placement="end"
-          >
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                Offcanvas
+                <img style={imgstyle}
+                  src={logo}
+                  width="100"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt=""
+                />
               </Offcanvas.Title>
             </Offcanvas.Header>
+
             <Offcanvas.Body>
-              
+
               <Nav className="justify-content-end flex-grow-1 pe-3 ">
-              <Form className="d-flex ">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
+                <Form className="d-flex ">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  
+                </Form>
+                <Nav.Link href="#features">Marketplace</Nav.Link>
+                <Nav.Link href="/contests" target="_blank">Contests</Nav.Link>
+                <ConnectButton
+                  bgColor="black"
+                  color="black"
+                  fontWeigth="800"
                 />
-                
-              </Form>
-              <Nav.Link href="#features">Marketplace</Nav.Link>
-            <Nav.Link href="#features">Contests</Nav.Link>
-            
-               
               </Nav>
-              
+
             </Offcanvas.Body>
           </Navbar.Offcanvas>
-          <ConnectButton />
         </Container>
       </Navbar>
-    
-  </>
-);
+      
+    </>
+  );
 }
 
 export default Navbar2;
---
+++ src/contests.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MoralisProvider } from "react-moralis";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Contests = () => {
+  return (
+    <div>
+      <h1>
+        GeeksforGeeks is a Computer 
+        Science portal for geeks.
+      </h1>
+    </div>
+  );
+};
+  
+export default Contests;
