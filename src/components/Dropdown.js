421428c51bcaebe8453ba36ba646be9f58c4e0d7
joaofbfrade <joaofbfrade97@gmail.com>
2022-10-25 16:52:26 +0100

    a
    aa

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -16,7 +16,7 @@ const Dropdown = () => {
           Options
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
-      </div>
+      </div >
 
       <Transition
         as={Fragment}
