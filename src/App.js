d41bdc03b4579fc8dbf387ef5d6ef22cd48a2c76
Pedro Bingre Miguel <pedro.bingre.miguel@gmail.com>
2022-11-07 13:02:42 +0000

    Admin Page Updates

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 import { MoralisProvider } from "react-moralis";
+import { useMoralis } from "react-moralis";
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useState, useRef, useEffect  } from 'react';
 
 import Navbar1 from './components/Navbar1';
 import Welcome from './pages/welcome';
@@ -13,6 +15,35 @@ import Admin from './pages/Admin';
 import IndividualContest from './pages/individualcontest';
 
 const App = () => {
+
+    // const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+
+    // useEffect(() => {
+    //     if (isAuthenticated) {
+    //     // add your logic here
+    //     }
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [isAuthenticated]);
+
+    // const login = async () => {
+    //   if (!isAuthenticated) {
+
+    //     await authenticate({signingMessage: "Log in using Moralis" })
+    //       .then(function (user) {
+    //         console.log("logged in user:", user);
+    //         console.log(!user.get("ethAddress"));
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error);
+    //       });
+    //   }
+    // }
+
+    // const logOut = async () => {
+    //   await logout();
+    //   console.log("logged out");
+    // }
+
     return (
         <>
             <Router>
@@ -28,8 +59,15 @@ const App = () => {
                     
                     </Routes>
 
+                    {/* <div>
+                        <h1>Moralis Hello World!</h1>
+                        <button onClick={login}>Moralis Metamask Login</button>
+                        <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
+                    </div> */}
+
                 </MoralisProvider>
             </Router>
+
         </>
     );
 }
