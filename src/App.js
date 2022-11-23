12c02c74a5cb5218bdcc748281c4c39969486ce7
Pedro Bingre Miguel <pedro.bingre.miguel@gmail.com>
2022-11-23 20:53:21 +0000

    testes ipfs (sem sucesso)

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Welcome from './pages/welcome';
 import Contests from './pages/contests';
 import Admin from './pages/Admin';
 
+import IpfsTest from './pages/ipfsTest';
 import IndividualContest from './pages/individualcontest';
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
                         <Route exact path='/' element={<Welcome />} />
                         <Route exact path='/contests' element={<Contests/>} />
                         {/* <Route exact path='/marketplace' element={<Marketplace/>} /> */}
+                        <Route exact path='/ipfsTest' element={<IpfsTest/>} />
                         <Route exact path='/individualcontest' element={<IndividualContest/>} />
                         <Route exact path='/Admin' element={<Admin />} />
                     
---
+++ src/pages/ipfsTest.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useState, useRef, useEffect } from 'react';
+
+// import { ImageUpload } from "react-ipfs-uploader";
+// import { create, CID, IPFSHTTPClient } from "ipfs-http-client";
+
+// import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
+
+
+
+const ipfsTest = () => {
+
+    // const { Moralis } = useMoralis();
+    // Moralis.enableWeb3();
+    // const fs = require("fs");
+
+    // async function uploadToIpfs(){
+
+    //     await Moralis.start({
+    //         apiKey: "PUT IT HERE"
+    //     });
+
+    //     const uploadArray = [
+    //         {
+    //             path: "oi.png",
+    //             content: fs.readFileSync('./oi.png', {encoding: 'base64'})
+    //         },
+    //         {
+    //             path: "out2.png",
+    //             content: {
+    //                 one: "red Lobster",
+    //                 two: "blue lobster",
+    //                 three: "green lobster"
+    //             }
+    //         },
+    //     ];
+
+    //     const response = await Moralis.EvmApi.ipfs.uploadFolder({
+    //         abi: uploadArray,
+    //     });
+
+    // }
+    
+    // const [imageUrl, setImageUrl] = useState("");
+
+    return (  
+
+        // <div>
+        //     <ImageUpload setUrl={setImageUrl} />
+        //     ImageUrl :{" "}
+        //     <a href={imageUrl} target="_blank" rel="noopener noreferrer">
+        //         {imageUrl}
+        //     </a>
+        // </div>
+        <div>oi</div>
+
+
+    );
+}
+ 
+export default ipfsTest;
