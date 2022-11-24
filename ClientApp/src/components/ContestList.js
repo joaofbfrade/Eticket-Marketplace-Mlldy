e500b38854b3fe8edcda894bad8a52e3bbe3b80f
Daniel <danniribeiro01@gmail.com>
2022-11-24 20:34:31 +0000

    Onclick function

--- ClientApp/src/components/ContestList.js
+++ ClientApp/src/components/ContestList.js
@@ -3,7 +3,7 @@
 const ContestList = ({ contests,filter,options }) => {
 
   // const contests = props.contests;
-
+   
 
   return (
 
@@ -11,11 +11,13 @@ const ContestList = ({ contests,filter,options }) => {
       className="rounded-2xl grid grid-cols-2 sm:grid-cols-4  place-content-center p-10 gap-4 text-center ">
 
           {contests.map((contest) => (
-
-              <div id="imgcont" className='relative' onClick={location.href = 'individualcontest/'+contest.hash} key={contest.id} >
+              <button onClick={() => { location.href = 'individualcontest/' + contest.hash }}>
+              <div id="imgcont" className='relative'  key={contest.id} >
+                 
           <img  id ="img1" className=" rounded-lg  w-full h-60 object-cover    " src={contest.img}  />
           <h3 className='absolute  text-white bottom-1 left-1/2 -translate-x-1/2 ' >{contest.musictype} </h3>      
-        </div>
+              </div>
+              </button>
 
       ))}
 
